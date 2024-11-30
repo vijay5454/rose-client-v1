b0b1b6b56f600bfa3bcc5b98a38fc9572ffdc289
vijay5454 <vijaythalaiva5454@gmail.com>
2024-11-30 10:19:31 +0530

    Edit bottom navbar

--- src/components/BottomNavbar.tsx
+++ src/components/BottomNavbar.tsx
@@ -9,14 +9,14 @@ import { GiHolySymbol } from "react-icons/gi";
 const BottomNavbar = () => {
   return (
     <div className="h-20 bg-primary fixed bottom-0 w-full md:hidden px-1">
-      <div className="flex justify-between items-center h-full text-xs">
+      <div className="flex justify-between items-center text-xs h-full">
         <NavLink
           to="/"
           className={({ isActive }) =>
-            isActive ? "bg-secondary rounded-lg" : ""
+            isActive ? "bg-secondary rounded-lg w-[16%] p-1" : "w-[16%] p-1"
           }
         >
-          <div className="flex flex-col gap-2 items-center py-2 px-1">
+          <div className="flex flex-col gap-2 items-center">
             <MdHomeFilled className="w-5 h-5" />
             <p>Home</p>
           </div>
@@ -24,10 +24,10 @@ const BottomNavbar = () => {
         <NavLink
           to="/prayers"
           className={({ isActive }) =>
-            isActive ? "bg-secondary rounded-lg" : ""
+            isActive ? "bg-secondary rounded-lg w-[16%] p-1" : "w-[16%] p-1"
           }
         >
-          <div className="flex flex-col gap-2 items-center py-2 px-1">
+          <div className="flex flex-col gap-2 items-center">
             <PiHandsPrayingDuotone className="w-5 h-5" />
             <p>Prayers</p>
           </div>
@@ -35,32 +35,38 @@ const BottomNavbar = () => {
         <NavLink
           to="/holy-mass"
           className={({ isActive }) =>
-            isActive ? "bg-secondary rounded-lg" : ""
+            isActive ? "bg-secondary rounded-lg w-[16%] p-1" : "w-[16%] p-1"
           }
         >
-          <div className="flex flex-col gap-2 items-center py-2 px-1">
+          <div className="flex flex-col gap-2 items-center">
             <PiChurch className="w-5 h-5" />
-            <p>Holy Mass</p>
+            <div className="text-center">
+              <p>Holy</p>
+              <p>Mass</p>
+            </div>
           </div>
         </NavLink>
         <NavLink
           to="/holy-eucharist"
           className={({ isActive }) =>
-            isActive ? "bg-secondary rounded-lg" : ""
+            isActive ? "bg-secondary rounded-lg w-[16%] p-1" : "w-[16%] p-1"
           }
         >
-          <div className="flex flex-col gap-2 items-center py-2 px-1">
+          <div className="flex flex-col gap-2 items-center">
             <LuChurch className="w-5 h-5" />
-            <p>Holy Eucharist</p>
+            <div className="text-center">
+              <p>Holy</p>
+              <p>Eucharist</p>
+            </div>
           </div>
         </NavLink>
         <NavLink
           to="/saints"
           className={({ isActive }) =>
-            isActive ? "bg-secondary rounded-lg" : ""
+            isActive ? "bg-secondary rounded-lg w-[16%] p-1" : "w-[16%] p-1"
           }
         >
-          <div className="flex flex-col gap-2 items-center py-2 px-1">
+          <div className="flex flex-col gap-2 items-center">
             <GiHolySymbol className="w-5 h-5" />
             <p>Saints</p>
           </div>
@@ -68,10 +74,10 @@ const BottomNavbar = () => {
         <NavLink
           to="/bible"
           className={({ isActive }) =>
-            isActive ? "bg-secondary rounded-lg" : ""
+            isActive ? "bg-secondary rounded-lg w-[16%] p-1" : "w-[16%] p-1"
           }
         >
-          <div className="flex flex-col gap-2 items-center py-2 px-1">
+          <div className="flex flex-col gap-2 items-center">
             <FaBible className="w-5 h-5" />
             <p>Bible</p>
           </div>
